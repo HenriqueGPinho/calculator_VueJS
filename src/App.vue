354f75bc79176da1e7412a1a063461bc15d4682a
Henrique Pinho <henriquegravino@outlook.com>
2024-02-29 22:16:58 -0300

    add: calculate function

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,27 @@
   import { reactive } from 'vue';
 
   const state = reactive({
-    firstValue: 0,
-    secondValue: 0,
-    operator: 'addition',
-    result: 0
+    firstValue: '',
+    secondValue: '',
+    operator: 'division',
+    result: ''
   })
+
+  const calculate = () => {
+    const { operator } = state;
+
+    switch (operator) {
+      case 'subtraction':
+        return state.result = state.firstValue - state.secondValue;
+      case 'multiplication':
+        return state.result = state.firstValue * state.secondValue;
+      case 'division':
+        return state.result = state.firstValue / state.secondValue;
+      default:
+        console.log(operator)
+        return state.result = state.firstValue + state.secondValue;
+    }
+  }
 
 </script>
 
@@ -16,16 +32,16 @@
       <h1>Calculadora VueJS</h1>
     </header>
     <form class="row justify-content-evenly">
-      <input @keyup="e => state.firstValue = e.target.value" type="number" placeholder="valor 1" class="m-1 col-3">
+      <input @keyup="e => {state.firstValue = parseFloat(e.target.value); calculate()}" type="number" placeholder="valor 1" class="m-1 col-3">
       <select class="m-1 col-3">
         <option value="addition">+</option>
         <option value="subtraction">-</option>
         <option value="multiplication">x</option>
         <option value="division">:</option>
       </select>
-      <input type="number" placeholder="valor 2" class="m-1 col-3">
+      <input @keyup="e => {state.secondValue = parseFloat(e.target.value); calculate()}" type="number" placeholder="valor 2" class="m-1 col-3">
     </form>
-    <h2 class="p-3 mt-3 mb-3 text-center bg-light rounded-3">{{ state.firstValue }}</h2>
+    <h2 class="p-3 mt-3 mb-3 text-center bg-light rounded-3">{{ state.result }}</h2>
   </div>
 </template>
 
