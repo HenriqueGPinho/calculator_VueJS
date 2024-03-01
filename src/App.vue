1bf5b4a8bb9badea73342a971ea942f6afbca9de
Henrique Pinho <henriquegravino@outlook.com>
2024-02-29 21:41:00 -0300

    add: state

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <script setup>
+  import { reactive } from 'vue';
+
+  const state = reactive({
+    firstValue: 0,
+    secondValue: 0,
+    operator: 'addition',
+    result: 0
+  })
 
 </script>
 
 <template>
   <div class="container">
-    <header class="p-5 mt-3 mb-3 text-center bg-light rounded-3">
+    <header class="p-4 mt-3 mb-3 text-center bg-light rounded-3">
       <h1>Calculadora VueJS</h1>
     </header>
-    <form class="row">
-      <input type="number" name="number1" id="number1" placeholder="primeiro valor" class="col-5">
-      <select class="col-2">
+    <form class="row justify-content-evenly">
+      <input @keyup="e => state.firstValue = e.target.value" type="number" placeholder="valor 1" class="m-1 col-3">
+      <select class="m-1 col-3">
         <option value="addition">+</option>
         <option value="subtraction">-</option>
         <option value="multiplication">x</option>
         <option value="division">:</option>
       </select>
-      <input type="number" name="number2" id="number2" placeholder="segundo valor" class="col-5">
+      <input type="number" placeholder="valor 2" class="m-1 col-3">
     </form>
-    <h2 class="p-3 mt-3 mb-3 text-center bg-light rounded-3">25</h2>
+    <h2 class="p-3 mt-3 mb-3 text-center bg-light rounded-3">{{ state.firstValue }}</h2>
   </div>
 </template>
 
